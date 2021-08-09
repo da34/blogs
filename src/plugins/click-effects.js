5c195b4b33840972680d0eba59628ae14432c8ac
Your Name <1255029618@qq.com>
2021-08-09 22:59:10 +0800

    添加点击 显示特效

--- src/plugins/click-effects.js
+++ src/plugins/click-effects.js
@@ -1,4 +1,21 @@
-// const arrs = ['❤富强❤', '❤民主❤', '❤文明❤', '❤和谐❤', '❤自由❤', '❤平等❤', '❤公正❤', '❤法治❤', '❤爱国❤', '❤敬业❤', '❤诚信❤', '❤友善❤']
-document.querySelector('body').addEventListener('click', function () {
-  console.log(1231321321321)
+const arr = ['❤富强❤', '❤民主❤', '❤文明❤', '❤和谐❤', '❤自由❤', '❤平等❤', '❤公正❤', '❤法治❤', '❤爱国❤', '❤敬业❤', '❤诚信❤', '❤友善❤']
+
+document.documentElement.addEventListener('click', function (e) {
+  const index = ~~(Math.random() * 12)
+  const spanDom = document.createElement('span')
+  const x = e.pageX
+  const y = e.pageY
+  spanDom.textContent = arr[index]
+  spanDom.className = 'text-popup'
+  spanDom.style = `
+    top: ${y - 20}px;
+    left: ${x}px;
+    color: rgb(${~~(255 * Math.random())},${~~(255 * Math.random())},${~~(255 * Math.random())})
+  `
+  this.appendChild(spanDom)
+
+  // 动画结束后删除自己
+  spanDom.addEventListener('animationend', function () {
+    spanDom.parentNode.removeChild(spanDom)
+  })
 })
