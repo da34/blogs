c9dd3d9410aa4ed971c795f49b5f6c784d0e09d1
994AK <1505398065@qq.com>
2022-07-30 11:56:05 +0800

    fix：修复小屏幕的情况下,个人信息益出问题

--- client/src/layouts/AppMain.vue
+++ client/src/layouts/AppMain.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="xl:max-w-screen-xl relative m-auto mt-20">
+  <div class="md:flex md:justify-between xl:max-w-screen-xl relative m-auto mt-20">
     <div class="w-[950px] max-w-full relative">
       <nuxt keep-alive />
     </div>
-    <div class="absolute top-0 right-0 w-[19rem] hidden xl:block">
+    <div class="sticky top-0 right-0 w-[19rem] hidden xl:block">
       <SideInfo />
     </div>
   </div>
