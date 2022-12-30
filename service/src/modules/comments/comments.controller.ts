032fce195ef72fb704cb301c9139d0e2365bac22
罗世玉 <luoshiyu@uniteman.com>
2022-12-30 17:31:15 +0800

    chore(service): 遗漏文件提交

---
+++ service/src/modules/comments/comments.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+
+@Controller('comments')
+export class CommentsController {
+  constructor(private readonly commentsService: CommentsService) {}
+
+  @Post()
+  create(@Body() createCommentDto: CreateCommentDto) {
+    return this.commentsService.create(createCommentDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.commentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.commentsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
+    return this.commentsService.update(+id, updateCommentDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.commentsService.remove(+id);
+  }
+}
---
+++ service/src/modules/comments/comments.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UpdateCommentDto } from './dto/update-comment.dto';
+
+@Injectable()
+export class CommentsService {
+  create(createCommentDto: CreateCommentDto) {
+    return 'This action adds a new comment';
+  }
+
+  findAll() {
+    return `This action returns all comments`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} comment`;
+  }
+
+  update(id: number, updateCommentDto: UpdateCommentDto) {
+    return `This action updates a #${id} comment`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} comment`;
+  }
+}
